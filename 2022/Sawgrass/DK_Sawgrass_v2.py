83fd6c07191f9984d85a3b04b3d1cefe486e005d
Sean Raymor <seanraymor@seans-mbp.myfiosgateway.com>
2022-03-09 09:43:43 -0500

    PGA Championship

--- 2022/Sawgrass/DK_Sawgrass_v2.py
+++ 2022/Sawgrass/DK_Sawgrass_v2.py
@@ -2,6 +2,7 @@
 # coding: utf-8
 
 ##libraries
+from itertools import count
 import warnings
 import sys
 import numpy as np
@@ -14,7 +15,7 @@ warnings.simplefilter(action='ignore', category=Warning)
 
 
 #user input
-iterations = 300000
+iterations = 1000000
 
 #define variables
 maxIter = 0                                    #placeholder for best lineup
@@ -23,8 +24,8 @@ j = 0                                          #top tier iterable
 k = 0                                          #top tier lineupdf iterable
 topTierLineup = pd.DataFrame(columns=['Player1','Player2','Player3','Player4','Player5','Player6','TOT'])
 
-path = '2022/BayHill/'
-name = 'DKSalaries-BayHill.csv'
+path = '2022/Sawgrass/'
+name = 'DKSalaries-Sawgrass.csv'
 df = DK_csv_assignemnt(path, name)
 scope, creds, client = google_credentials()
 course_df = assign_course_df(client)
@@ -55,11 +56,9 @@ General:
 df = drop_players_lower_than(df, 6200)
 df_merge = weight_efficiencies(df, course_df)
 df_merge = pga_odds(df_merge)
-df_merge = course_fit(df_merge)
+df_merge = course_fit(df_merge, multiplier=20)
 df_merge = past_results(df_merge, 'https://www.espn.com/golf/leaderboard/_/tournamentId/401243005', upperBound=4)
 df_merge = past_results(df_merge, 'https://www.espn.com/golf/leaderboard/_/tournamentId/401056522', upperBound=2, pr_i=1)
-
-#delete_unused_columns(df_merge)
 
 df_merge.to_csv('{}/CSVs/DKData.csv'.format(path), index = False) #optional line if you want to see data in CSV
 
@@ -68,25 +67,21 @@ df_merge = df_total_and_reformat(df_merge)
 df_merge.to_csv('{}/CSVs/DKFinal.csv'.format(path), index = False) #optional line if you want to see data in CSV
 
 
+
 #Calculate Mean and Standard Deviation
 mean = df_merge['Total'].mean()*6
 sigma = df_merge['Total'].std()*6
 
 print(df_merge.head())
 
-while i < iterations:
+while k < 165:
     #get a sample
     lineup = genIter(df_merge)
     lineup.sort()
     #assign sample
     currentIter = objective(lineup, df_merge)
-    #check if sample is better than current best sample
-    if currentIter > maxIter and constraint(lineup, df_merge):
-        #reassign
-        maxIter = currentIter
-        maxLineup = lineup
     #check if sample is a top tier sample
-    if currentIter > (mean + sigma - 5) and constraint(lineup, df_merge):
+    if currentIter > (mean + sigma) and constraint(lineup, df_merge):
         #add players to top tier dataframe
         topTierData = getNames(lineup, df_merge)
         topTierData.append(currentIter)
@@ -100,14 +95,16 @@ while i < iterations:
     if i % 1000 == 0:
         print(i)
 
+
+
 #optimize data after loop
 OptimizedLineup = optimize_main(topTierLineup, df_merge)
 MaximizedLineup = maximize_main(OptimizedLineup, df_merge)
 MaximizedLineup = remove_outliers_main(MaximizedLineup, df_merge)
-topPlayers = find_top_players_for(MaximizedLineup, df_merge)
+MaximizedLineup = optimize_ownership(MaximizedLineup, df_merge)
+countPlayers = calculate_oversub_count(MaximizedLineup, df_merge, csv=True)
 
-
-topPlayers.to_csv('{}CSVs/Top_Players.csv'.format(path),index=False)
+countPlayers.to_csv('{}CSVs/Player_Ownership.csv'.format(path, index=False))
 MaximizedLineup.to_csv('{}CSVs/Maximized_Lineups.csv'.format(path),index=False)
 
 print(MaximizedLineup.head())
