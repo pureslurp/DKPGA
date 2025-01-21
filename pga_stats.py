6dad36c45f84a6753b76a6475109dc4686727e7e
PureSlurp <sraymor17@gmail.com>
2025-01-20 19:39:28 -0500

    farm monday

--- pga_stats.py
+++ pga_stats.py
@@ -285,5 +285,5 @@ def create_pga_stats(tourney: str, weights: dict = None):
         print(f"Stats file already exists at: {stats_path}")
 
 if __name__ == "__main__":
-    tourney = "The_American_Express"
+    tourney = "Farmers_Insurance_Open"
     create_pga_stats(tourney)
