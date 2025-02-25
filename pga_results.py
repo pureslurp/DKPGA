af9a6a183f637b086a60a7b485661d7e79f0a7f8
PureSlurp <sraymor17@gmail.com>
2025-02-24 21:53:39 -0500

    back test finish

--- pga_results.py
+++ pga_results.py
@@ -1,5 +1,5 @@
 from utils import TOURNAMENT_LIST_2025
-from Legacy.pga_dk_scoring import dk_points_df
+from backtest.pga_dk_scoring import dk_points_df
 from pga_v5 import fix_names
 import pandas as pd
 
