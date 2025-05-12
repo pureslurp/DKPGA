fd5c85c8f2c94de3a7dad68ff2aebb9cf2000d05
PureSlurp <sraymor17@gmail.com>
2025-05-12 19:37:35 -0400

    truist

--- pga_stats.py
+++ pga_stats.py
@@ -285,5 +285,5 @@ def create_pga_stats(tourney: str, weights: dict = None):
         print(f"Stats file already exists at: {stats_path}")
 
 if __name__ == "__main__":
-    tourney = "THE_CJ_CUP_Byron_Nelson"
+    tourney = "Truist_Championship"
     create_pga_stats(tourney)
